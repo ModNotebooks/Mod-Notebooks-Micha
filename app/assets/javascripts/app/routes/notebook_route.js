962c2eeccc9916b57b84fcbdaf7a25cd0e39ea1b
Eric Kreutzer <eric@airkrft.com>
2014-02-04 17:37:27 -0700

    Dont show the UI until both the notebook and pages are loaded

--- app/assets/javascripts/app/routes/notebook_route.js
+++ app/assets/javascripts/app/routes/notebook_route.js
@@ -1,6 +1,18 @@
 App.NotebookRoute = Ember.Route.extend(Ember.SimpleAuth.AuthenticatedRouteMixin, {
   model: function(params) {
-    return this.store.find('notebook', params.notebook_id);
+    var store = this.store;
+
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      store.find('notebook', params.notebook_id).then(function(notebook) {
+        notebook.get('pages').then(function(pages) {
+          resolve(notebook);
+        }, function(err) {
+          reject(err);
+        })
+      }, function(err) {
+        reject(err);
+      })
+    });
   },
 
   renderTemplate: function() {
