50a8c84d67184987d75fe4cd4f99d8c56350f097
Eric Kreutzer <eric@airkrft.com>
2014-02-06 17:43:51 -0700

    PageFiller tweaks

--- config/initializers/grim.rb
+++ config/initializers/grim.rb
@@ -1,6 +1,6 @@
-if %w(staging production).include?(Rails.env)
-  Grim.processor = Grim::MultiProcessor.new([
-    Grim::ImageMagickProcessor.new({ ghostscript_path: "/app/vendor/gs/gs/bin/gs" }),
-    Grim::ImageMagickProcessor.new({ ghostscript_path: "/usr/bin/gs"})
-  ])
-end
+# if %w(staging production).include?(Rails.env)
+#   Grim.processor = Grim::MultiProcessor.new([
+#     Grim::ImageMagickProcessor.new({ ghostscript_path: "/app/vendor/gs/gs/bin/gs" }),
+#     Grim::ImageMagickProcessor.new({ ghostscript_path: "/usr/bin/gs"})
+#   ])
+# end
