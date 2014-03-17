fb34060bef222f6f5858c36673a641458a91eea5
Eric Kreutzer <eric@airkrft.com>
2014-03-16 22:41:35 -0600

    More tweaks. Remove the ember debug shit in prod

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,13 +44,7 @@
     }
   });
 
-  Ember.Application.reopen({
-    LOG_ACTIVE_GENERATION: true,
-    LOG_MODULE_RESOLVER: true,
-    LOG_TRANSITIONS: true,
-    LOG_TRANSITIONS_INTERNAL: true,
-    LOG_VIEW_LOOKUPS: true
-  });
+  Ember.Application.reopen(window.ENV.APPLICATION_OPTIONS || {});
 
   var MainApp = Ember.Application.create({
     name: 'main',
