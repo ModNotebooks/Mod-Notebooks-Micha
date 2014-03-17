f3d958cdce53336237d0178915f4f57637b1f194
Eric Kreutzer <eric@airkrft.com>
2014-03-16 21:23:55 -0600

    Dont access notebooks in migration

--- db/migrate/20131220032118_add_pages_count_to_notebook.rb
+++ db/migrate/20131220032118_add_pages_count_to_notebook.rb
@@ -1,10 +1,5 @@
 class AddPagesCountToNotebook < ActiveRecord::Migration
   def change
     add_column :notebooks, :pages_count, :integer, default: 0
-
-    Notebook.reset_column_information
-    Notebook.all.each do |n|
-      Notebook.update_counters n.id, pages_count: n.pages.length
-    end
   end
 end
