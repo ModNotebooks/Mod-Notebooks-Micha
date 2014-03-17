b86103b9b294ea8de34df59fd14104b4e2ac0081
Eric Kreutzer <eric@airkrft.com>
2014-03-16 21:35:17 -0600

    Asset env

--- app/controllers/app/base_controller.rb
+++ app/controllers/app/base_controller.rb
@@ -1,5 +1,5 @@
 class App::BaseController < ApplicationController
-  if Rails.env.staging?
+  if ENV['REQUIRE_HTTP_AUTH'].present?
     http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_USERNAME'], password: ENV['HTTP_BASIC_AUTH_PASSWORD']
   end
 end
