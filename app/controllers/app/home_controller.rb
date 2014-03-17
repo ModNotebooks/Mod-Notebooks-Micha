f58b31523b21c2aeb48a619fc58905eac50e83f7
Eric Kreutzer <eric@airkrft.com>
2014-03-16 22:32:20 -0600

    More changes

--- app/controllers/app/home_controller.rb
+++ app/controllers/app/home_controller.rb
@@ -3,12 +3,4 @@ class App::HomeController < App::BaseController
   layout "application-app"
 
   def index; end
-
-  def guide
-    render 'guide', layout: false
-  end
-
-  def layout
-    render 'layout', layout: false
-  end
 end
