2bda730b7fa1c71a08a08317d7088723c0897c6f
Eric Kreutzer <eric@airkrft.com>
2014-03-20 04:14:38 -0600

    Remove all the password reset stuff we were doing with Ember

--- app/assets/javascripts/app/views/auth_views.js
+++ app/assets/javascripts/app/views/auth_views.js
@@ -7,8 +7,3 @@ App.SignupView = Ember.View.extend({
   classNames: ['l-guest-main'],
   templateName: 'login'
 });
-
-App.PasswordResetView = Ember.View.extend({
-  classNames: ['l-guest-main'],
-  templateName: 'login'
-});
