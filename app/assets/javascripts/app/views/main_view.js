f711b7786428817cfa47f8fb20b197387b563301
Eric Kreutzer <eric@airkrft.com>
2014-03-27 19:32:51 -0600

    Tweaks

--- app/assets/javascripts/app/views/main_view.js
+++ app/assets/javascripts/app/views/main_view.js
@@ -1,7 +1,3 @@
 App.MainView = Ember.View.extend({
-  classNames: ['l-app-main'],
-
-  noScroll: function() {
-    $(document.documentElement).toggleClass('no-scroll', this.get('controller.settingsVisible'));
-  }.observes('controller.settingsVisible')
+  classNames: ['l-app-main']
 });
