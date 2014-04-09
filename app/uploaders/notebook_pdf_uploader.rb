86442bb8948f2cd6fb031adfca411ded453e3f01
Eric Kreutzer <eric@airkrft.com>
2014-04-09 15:45:47 -0600

    NotebookPDFUpload changes

--- app/uploaders/notebook_pdf_uploader.rb
+++ app/uploaders/notebook_pdf_uploader.rb
@@ -1,9 +1,15 @@
-class NotebookPDFUploader < CarrierWave::Uploader::Base
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+class NotebookPDFUploader < BaseUploader
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
   end
 
   def extension_white_list
     %w(pdf)
   end
+
+  protected
+    def secure_token
+      var = :"@#{mounted_as}_secure_token"
+      model.instance_variable_get(var) or model.instance_variable_set(var, super)
+    end
 end
